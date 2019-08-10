1cddb9d0592e1264d03f099dafed8fcca6667873
sample <dorothy_007@naver.com>
2019-08-10 15:51:11 +0900

    고객 컴포넌트 내용 분리

--- src/App.js
+++ src/App.js
@@ -2,21 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
-  'name': '홍길동',
-  'birthday': '961222',
-  'gender': '남자',
-  'job': '대학생'
-}
-  
+const customer = [
+  {
+    'id': 1,
+    'image': 'http://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '961222',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '나동빈',
+    'birthday': '960508',
+    'gender': '남자',
+    'job': '프로그래머'
+    },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '이순신',
+    'birthday': '961127',
+    'gender': '남자',
+    'job': '디자이너'
+  }
+]
   
 
 class App extends Component {
   render() {
     return (
-     <Customer 
-        customer={customer}
-     />
+      <div>
+        <Customer customer={customer} />
+      </div>
     );
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -3,16 +3,35 @@ import React, { Component } from 'react';
 class Customer extends Component {
     render() {
         const { customer } = this.props;
-        const { name, birthday, gender, job } = customer
         return (
             <div>
-                <h2>{name}</h2>
-                <p>{birthday}</p>
-                <p>{gender}</p>
-                <p>{job}</p>
+                <CustomerProfile customer={customer} />         
             </div>
         );
     }
 }
+
+class CustomerProfile extends Component {
+    render() {
+        const { customer } = this.props;
+        const customerProfile = customer.map(
+            custom => (
+            <div key={custom.id}>
+                <img src={custom.image} alt="profile"/>
+                <h2>{custom.name}({custom.id})</h2>
+                <p>{custom.birthday}</p>
+                <p>{custom.gender}</p>
+                <p>{custom.job}</p>
+            </div>
+            )
+        );
+        return (
+            <div>
+                {customerProfile}
+            </div>
+        );
+    }
+}
+
 
 export default Customer;
